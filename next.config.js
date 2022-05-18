f0df5104e99ba93e2a31baa4e35149e14b4f7cad
Dani Manalang <dani@theplate.org>
2022-05-18 11:39:52 +0800

    Add image and device sizes in config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   env: {
     API: 'https://kageapp.io',
   },
+  images: {
+    deviceSizes: [325, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [8, 16, 32, 48, 64, 96, 128, 256, 384],
+  }
 }
